28c3dc41aeff3b582eeb7d9b8de886c5efe928a9
Roxel Lay <contact@romukulot.fr>
2020-05-02 16:05:18 +0200

    MAJ des éléments dans les fichiers .gitignore ; corr page 404 dans le front controller legacy ; suppr de fichiers dans /storage ajoutés par erreur.

--- public/front.php
+++ public/front.php
@@ -1,41 +1,38 @@
 <?php
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 require_once('../Connections/maconnexion.php');
 
 if($path === '') {
     $_GET['target'] = 'index';
 }
 
-if(isset($_GET['target'])) {
-
-    $mondegc_config['front-controller'] = array();
-
-    $mondegc_config['front-controller']['enabled'] = true;
-
-    $mondegc_config['front-controller']['path'] = filter_filename($_GET['target']);
-
-    $mondegc_config['front-controller']['path'] = str_replace('.', '/', $mondegc_config['front-controller']['path']);
-
-    $mondegc_config['front-controller']['require'] = DEF_ROOTPATH . $mondegc_config['front-controller']['path'] . '.php';
-
-    if($mondegc_config['env'] !== 'production') {
-        getErrorMessage('success',
-            "Using front controller (PHP " . phpversion() . ")<br>Required path: " . $mondegc_config['front-controller']['require']);
-    }
-
-    ob_start();
-    if(file_exists($mondegc_config['front-controller']['require'])) {
-        @require($mondegc_config['front-controller']['require']);
-    }
-    
-    return csrf_ob_handler(ob_get_clean(), null);
-
-}
-
-else {
+if(!isset($_GET['target'])) {
     header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
-    echo "<!DOCTYPE html>
-    <html><head><title>Monde GC - Nope nope.</title></head>
-    <body><h1>404.</h1><p>Page non trouvée.</p></body>
-    </html>";
+    throw new NotFoundHttpException("Page non trouvée.");
 }
+
+$mondegc_config['front-controller'] = array();
+
+$mondegc_config['front-controller']['enabled'] = true;
+
+$mondegc_config['front-controller']['path'] = filter_filename($_GET['target']);
+
+$mondegc_config['front-controller']['path'] = str_replace('.', '/', $mondegc_config['front-controller']['path']);
+
+$mondegc_config['front-controller']['require'] = DEF_ROOTPATH . $mondegc_config['front-controller']['path'] . '.php';
+
+if($mondegc_config['env'] !== 'production') {
+    getErrorMessage('success',
+        "Using front controller (PHP " . phpversion() . ")<br>Required path: " . $mondegc_config['front-controller']['require']);
+}
+
+if(!file_exists($mondegc_config['front-controller']['require'])) {
+    throw new NotFoundHttpException("Page non trouvée.");
+}
+
+ob_start();
+@require($mondegc_config['front-controller']['require']);
+
+return csrf_ob_handler(ob_get_clean(), null);
