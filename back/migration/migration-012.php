a2a09b91563b4f515f528ea22cba9dbcac5bd53d
Roxel Lay <contact@romukulot.fr>
2020-07-31 13:14:43 +0200

    La vue BDD 'temperance_organisation' n'affiche plus les statistiques pour certaines organisations.
    Les organisations ayant le paramètre 'allow_temperance' à 0 ne voient plus leurs données économiques être calculées.
    
    Dans public/front.php, dans certains cas, certains paramètres dans la variable mondegc_config sont considérés comme non présents dans l'array... Bug incompréhensible mais workaround trouvé en ajoutant isset() aux paramètres posant problème.
    
    MAJ de paramètres dans config/models.php.

---
+++ back/migration/migration-012.php
@@ -0,0 +1,38 @@
+<?php
+
+/* *******************
+ * Script de migration
+ * Version cible : 2.7
+ * ******************/
+
+mysql_select_db($database_maconnexion, $maconnexion);
+
+$queries = array();
+
+$queries[] = "SET FOREIGN_KEY_CHECKS=0";
+
+$queries[] = "drop view if exists temperance_organisation";
+
+$queries[] = "CREATE VIEW `temperance_organisation` AS SELECT o.id, o.name,
+   SUM(budget) AS budget,
+   SUM(agriculture) AS agriculture,
+   SUM(commerce) AS commerce,
+   SUM(education) AS education,
+   SUM(environnement) AS environnement,
+   SUM(industrie) AS industrie,
+   SUM(recherche) AS recherche,
+   SUM(tourisme) AS tourisme
+FROM organisation_members
+INNER JOIN organisation o on organisation_members.organisation_id = o.id
+INNER JOIN pays p on organisation_members.pays_id = p.ch_pay_id
+LEFT JOIN temperance_pays tp on organisation_members.pays_id = tp.id
+WHERE organisation_members.permissions >= 10
+  AND o.allow_temperance = 1
+GROUP BY o.id";
+
+$queries[] = "SET FOREIGN_KEY_CHECKS=1";
+
+// Exécuter la requête
+foreach($queries as $query) {
+    mysql_query($query) or die(mysql_error());
+}
