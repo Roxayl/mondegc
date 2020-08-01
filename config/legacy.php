826e73a8b92e40a1f7e5872361565bae4518ce95
Roxel Lay <contact@romukulot.fr>
2020-08-01 12:51:06 +0200

    Fusion des fichiers de configuration legacy dans Laravel.
    Auparavant, il existait deux façons de configurer le site, dans Connections/config.*.php pour le site legacy, ou dans les fichiers d'environnement .env ou config/*.php pour l'app Laravel.
    
    Les fichiers de configuration legacy sont supprimés et on appelle plutôt les données via la fonction helper config() dans Connections/maconnexion.php pour définir ces données. On créé le fichier config/legacy.php afin de stocker ces informations.

---
+++ config/legacy.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'version' => env('LEGACY_VERSION', '2.x'),
+
+    'hide_errors' => env('LEGACY_HIDE_ERRORS', false),
+
+    'enable_csrf_protection' => env('LEGACY_CSRF', true),
+
+];
