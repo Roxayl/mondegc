a972bc02852481172dfa2dee7115e3e61d357e95
Roxel Lay <romuald.fabiani@outlook.fr>
2020-08-17 13:10:17 +0000

    Fusion effectuée bugfix/bugfix-004-responsive-style (pull request #16)
    Bugfix/bugfix 004 responsive style
    
    * Remplace e() par htmlPurify() dans le texte de présentation des villes.
    
    * Supprime le lien vers l'accueil sur la navbar.
    
    Indente un peu mieux le code de page-ville.php.
    
    * Améliore la vue mobile pour la navbar.
    
    * Indente le fichier /assets/css/GenerationCity.css et exécute l'inspection de code.
    
    * Compile les fichiers JS via Laravel Mix.
    
    On compile en deux fichiers : /public/js/vendor-compiled.js (jQuery, Bootstrap, etc.) et /public/js/application-compiled.js (l'application).
    
    * Améliore l'affichage des boîtes d'infras sur vue mobile.

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,6 +18,18 @@ mix.js('resources/js/app.js', 'public/js')
 mix.js(['resources/js/admin/admin.js'], 'public/js')
     .sass('resources/sass/admin/admin.scss', 'public/css');
 
+mix.scripts([
+    'assets/js/jquery.js',
+    'assets/js/bootstrap.js',
+    'assets/js/bootstrap-affix.js',
+    'assets/js/bootstrap-scrollspy.js',
+    'assets/js/bootstrapx-clickover.js',
+    'assets/js/bootstrap-modalmanager.js',
+    'assets/js/bootstrap-modal.js',
+], 'public/js/vendor-compiled.js');
+
+mix.scripts(['assets/js/application.js'], 'public/js/application-compiled.js');
+
 if (mix.inProduction()) {
     mix.version();
 }
