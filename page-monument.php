b946c586163bd642dc5ffe14d5e5e924d3664936
Roxel Lay <romuald.fabiani@outlook.fr>
2021-09-06 17:15:25 +0000

    Fusion effectuée feature/myname-modif-pages-et-inactivite (pull request #93)
    Modifications pour les entreprises
    
    * Calcul des ressources avec le coeff où visiblement crash avec la multiplication
    
    Test de modif des section directement depuis l'affichage final (section transports uniquement)
    
    Problèmes :
    
    - calcul des ressources avec le coeff où visiblement crash avec la multiplication
    - impossible de mettre à jour le contenu "dernières actualités" quand l'entreprise nous appartient
    
    * Merge branch 'develop' into feature/myname-modif-pages-et-inactivite
    
    * patrimoine.php edited online with Bitbucket2 bugs qui rendent les modif purement esthétiques : pb de MAJ de la section "dernières actualités" + non prise en compte de la dégressivité par l'inactivité
    * Boîte à outils du GO
    
    * Merge branch 'develop' into feature/myname-modif-pages-et-inactivite
    
    * Revert "patrimoine.php edited online with Bitbucket2 bugs qui rendent les modif purement esthétiques : pb de MAJ de la section "dernières actualités" + non prise en compte de la dégressivité par l'inactivité"
    
    This reverts commit 156a6aad

--- page-monument.php
+++ page-monument.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 //Connexion et deconnexion
 include('php/log.php');
 
@@ -9,7 +11,7 @@ if (isset($_GET['ch_pat_id'])) {
   $colname_monument = $_GET['ch_pat_id'];
 }
 
-$query_monument = sprintf("SELECT ch_pat_id, ch_pat_label, ch_pat_statut, ch_pat_paysID, ch_pat_villeID, ch_pat_date, ch_pat_mis_jour, ch_pat_nb_update, ch_pat_coord_X, ch_pat_coord_Y, ch_pat_nom, ch_pat_lien_img1, ch_pat_lien_img2, ch_pat_lien_img3, ch_pat_lien_img4, ch_pat_lien_img5, ch_pat_legende_img1, ch_pat_legende_img2, ch_pat_legende_img3, ch_pat_legende_img4, ch_pat_legende_img5, ch_pat_description, ch_pay_id, ch_pay_nom, ch_vil_ID, ch_vil_nom, (SELECT GROUP_CONCAT(ch_disp_cat_id) FROM dispatch_mon_cat WHERE ch_pat_ID = ch_disp_mon_id) AS listcat FROM patrimoine INNER JOIN pays ON ch_pat_paysID = ch_pay_id INNER JOIN villes ON ch_pat_villeID = ch_vil_ID WHERE ch_pat_id = %s", GetSQLValueString($colname_monument, "int"));
+$query_monument = sprintf("SELECT ch_pat_id, ch_pat_label, ch_pat_statut, ch_pat_paysID, ch_pat_villeID, ch_pat_date, ch_pat_mis_jour, ch_pat_nb_update, ch_pat_coord_X, ch_pat_coord_Y, ch_pat_nom, ch_pat_lien_img1, ch_pat_lien_img2, ch_pat_lien_img3, ch_pat_lien_img4, ch_pat_lien_img5, ch_pat_legende_img1, ch_pat_legende_img2, ch_pat_legende_img3, ch_pat_legende_img4, ch_pat_legende_img5, ch_pat_description, ch_pat_commentaire, ch_pay_id, ch_pay_nom, ch_vil_ID, ch_vil_nom, (SELECT GROUP_CONCAT(ch_disp_cat_id) FROM dispatch_mon_cat WHERE ch_pat_ID = ch_disp_mon_id) AS listcat FROM patrimoine INNER JOIN pays ON ch_pat_paysID = ch_pay_id INNER JOIN villes ON ch_pat_villeID = ch_vil_ID WHERE ch_pat_id = %s", GetSQLValueString($colname_monument, "int"));
 $monument = mysql_query($query_monument, $maconnexion) or die(mysql_error());
 $row_monument = mysql_fetch_assoc($monument);
 $totalRows_monument = mysql_num_rows($monument);
@@ -19,10 +21,23 @@ $query_monument_ressources = sprintf("SELECT SUM(ch_mon_cat_budget) AS budget,SU
   INNER JOIN dispatch_mon_cat ON dispatch_mon_cat.ch_disp_cat_id = monument_categories.ch_mon_cat_ID
   INNER JOIN patrimoine ON ch_pat_id = ch_disp_mon_id WHERE ch_pat_id = %s", GetSQLValueString($colname_monument, "int"));
 $monument_ressources = mysql_query($query_monument_ressources, $maconnexion) or die(mysql_error());
-$row_monument_ressources = mysql_fetch_assoc($monument_ressources);
+$row_monument_ressources_neutre = mysql_fetch_assoc($monument_ressources);
+
+
+        $lastActivity = $row_monument['ch_pat_mis_jour'];
+        $coefficient = 1 ;
+
+        if($lastActivity < Carbon::now()->subMonths(6)) {
+            $coefficient = 0.1;
+        }
+        elseif($lastActivity < Carbon::now()->subMonths(3)) {
+            $coefficient = 0.5;
+        }
+
+$row_monument_ressources_neutre['budget'] = $row_monument_ressources_neutre['budget'] * $coefficient;
+$row_monument_ressources = $row_monument_ressources_neutre ;
 
 // Connection infos dirigeant pays
-
 $query_users = sprintf("SELECT ch_use_id, ch_use_login FROM users WHERE ch_use_paysID = %s", GetSQLValueString($row_monument['ch_pat_paysID'], "int"));
 $users = mysql_query($query_users, $maconnexion) or die(mysql_error());
 $row_users = mysql_fetch_assoc($users);
@@ -32,7 +47,7 @@ $totalRows_users = mysql_num_rows($users);
 // *** Requête pour infos sur les categories.
 $listcategories = ($row_monument['listcat']);
 			if ($row_monument['listcat']) {
-          
+
 
 $query_liste_mon_cat3 = "SELECT * FROM monument_categories WHERE ch_mon_cat_ID In ($listcategories) ORDER BY ch_mon_cat_couleur";
 $liste_mon_cat3 = mysql_query($query_liste_mon_cat3, $maconnexion) or die(mysql_error());
@@ -134,6 +149,21 @@ if ($row_monument['listcat'])
    else {$nb_cat_ok = 1;}
 }
 
+// Mise a jour actu de l'entreprise
+$editFormAction = DEF_URI_PATH . $mondegc_config['front-controller']['path'] . '.php';
+appendQueryString($editFormAction);
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "ajouter_actu")) {
+  $updateSQL = sprintf("UPDATE patrimoine SET ch_pat_commentaire=%s WHERE ch_pat_id=%s",
+                       GetSQLValueString($_POST['ch_pat_commentaire'], "text"),
+                       GetSQLValueString($_POST['ch_pat_id'], "int"));
+
+      $Result1 = mysql_query($updateSQL, $maconnexion) or die(mysql_error());
+      getErrorMessage('success', "L'actualité de l'entreprise a été modifiée avec succès !");
+    } else {
+      getErrorMessage('error', "Une erreur est survenue :(");
+    }
+
 mysql_data_seek($ressource, 0);}
 
 ?><!DOCTYPE html>
@@ -141,7 +171,7 @@ mysql_data_seek($ressource, 0);}
 <!-- head Html -->
 <head>
 <meta charset="utf-8">
-<title>Monde GC - Quête : <?= __s($row_monument['ch_pat_nom']) ?></title>
+<title><?= __s($row_monument['ch_pat_nom']) ?> - Monde GC</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="author" content="">
@@ -185,7 +215,7 @@ mysql_data_seek($ressource, 0);}
 <script src="assets/js/bootstrap-scrollspy.js"></script>
 <script src="assets/js/bootstrapx-clickover.js"></script>
 <script type="text/javascript">
-      $(function() { 
+      $(function() {
           $('[rel="clickover"]').clickover();})
 </script>
 <!-- MODAL -->
@@ -202,7 +232,7 @@ mysql_data_seek($ressource, 0);}
 <!-- Navbar
     ================================================== -->
 <?php $pays=true; include('php/navbar.php'); ?>
-<header id="info-ville" class="jumbotron subhead anchor"> 
+<header id="info-ville" class="jumbotron subhead anchor">
   <!-- Titre  et Carousel
     ================================================== -->
   <div class="container container-carousel">
@@ -277,7 +307,7 @@ mysql_data_seek($ressource, 0);}
   <?php if ($row_users['ch_use_id'] == $_SESSION['user_ID']) { ?>
   <a class="btn btn-primary pull-right" href="php/partage-monument.php?ch_pat_id=<?= e($row_monument['ch_pat_id']) ?>" data-toggle="modal" data-target="#Modal-Monument" title="Poster sur le forum"><i class="icon-share icon-white"></i>Partager sur le forum</a>
   <?php } ?>
-  
+
   <div class="clearfix"></div>
   <div class="modal container fade" id="Modal-Monument"></div>
   <div class="titre-vert">
@@ -286,18 +316,53 @@ mysql_data_seek($ressource, 0);}
   <div class="well">
     <div class="row-fluid">
       <div>
-        <div class="alert alert-info">
-            <h4>Je suis en <span class="badge badge-warning">BETA</span></h4>
-            <p style="margin: 0px;">Les quêtes sont une fonctionnalité en cours de test. <a class=""  href="https://www.forum-gc.com/t7224-go-entreprises#292209">N'hésitez pas à faire vos retours sur le forum !</a></p>
-        </div>
 
         <p><img src="<?= __s($thisPays->get('ch_pay_lien_imgdrapeau')) ?>" class="img-menu-drapeau"> <a class="" href="page-pays.php?ch_pay_id=<?= e($row_monument['ch_pat_paysID']) ?>"><?= __s($row_monument['ch_pay_nom']) ?></a> • <?php if ($row_monument['ch_pat_statut']==0) { ?> Entreprise référencée à <?php } else { ?><?php }?>
         <a class="" href="page-ville.php?ch_pay_id=<?= e($row_monument['ch_pat_paysID']) ?>&ch_ville_id=<?= e($row_monument['ch_pat_villeID']) ?>"><?= __s($row_monument['ch_vil_nom']) ?></a></p>
-        <p><?= __s($row_monument['ch_pat_description']) ?></p>
+
+        <p><?= __s($row_monument['ch_pat_description']) ?></p><br>
+
+     <?php renderElement('errormsgs'); ?>
+        <!-- Dernières actualités -->
+        <?php if($row_monument['ch_mon_cat_statut'] == 0) { ?>
+
+        <div class="accordion-group">
+              <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" href="#spoiler-actu" style="background: #f0eeec;"><h4 style="margin-top: 0px; margin-bottom: 0px; text-transform: uppercase; font-weight: lighter; color:#101010; ">Dernières actualités de l'entreprise</h4><div class='' style="font-size: 13px;color: #577991;">Dernière mise à jour le <?php echo date("d/m/Y", strtotime($row_monument['ch_pat_mis_jour'])); ?></div></a>
+              </div>
+                <div id="spoiler-actu" class="accordion-body collapse">
+                  <div class="accordion-inner">
+                    <?php if($row_monument['ch_pat_commentaire']) { ?>
+                      <div style=><?= htmlPurify($row_monument['ch_pat_commentaire']) ?></div>
+                    <?php } else { ?><p style='font-style: italic;'>Rien à signaler pour le moment...</p><?php }?>
+
+           <?php if (($_SESSION['statut'] >= 20) OR ($row_users['ch_use_id'] == $_SESSION['user_ID'])) { ?>
+           <div class="accordion-group" style="border: none;">
+              <div class="accordion-heading">
+                <a class="accordion-toggle thumbnail btn-ajout" data-toggle="collapse" href="#spoiler-actu-modif" style="scale: 85%;margin: 0.5em 0em 0em -1em;width: 250px;">Ajouter une actualité</a>
+              </div>
+                <div id="spoiler-actu-modif" class="accordion-body collapse" style="height: 0px;">
+                  <div class="accordion-inner" style="scale: 0.95; border: 5px dotted rgb(255, 78, 0); border-radius: 15px; box-shadow: rgba(0, 0, 0, 0.1) 0px 8px 9px -5px, rgba(19, 5, 5, 0.04) 0px 15px 22px 2px, rgba(0, 0, 0, 0.1) 0px 6px 28px 5px !important; padding: 0em; margin-top: 0.2em;">
+                    <form action="<?= e($editFormAction) ?>" method="POST" class="form-horizontal" name="ajouter_actu" Id="ajouter_actu" style="margin: 0;">
+                    <textarea rows="20" name="ch_pat_commentaire" class="wysiwyg" id="ch_pat_commentaire" style="height: 200px;"><?= htmlPurify($row_monument['ch_pat_commentaire']) ?></textarea>
+                    <button type="submit" class="btn btn-primary" style="width: 96%; text-align: center; margin: 1em 1em; border-radius: 0px 0px 10px 10px;">Modifier cette section</button>
+                    <input type="hidden" name="MM_update" value="ajouter_actu">
+                   </form>
+                  </div>
+                </div>
+              </div>
+
+                  <?php } ?>
+                  </div></div>
+                </div><br><br>
 
 
+      <?php } ?>
+
+<?php if($nb_cat_ok !== 0) { ?>
         <!-- Liste des categories du monument -->
-        <p><strong><?php echo $nb_cat_ok ?> objectifs atteints jusqu'à présent&nbsp;:</strong></p>
+        <h3 class="souligne" style="margin-top: 0; border-color:#1a2638"><?php echo $nb_cat_ok ?> objectifs atteints jusqu'à présent&nbsp;</h3>
+
         <?php if ($row_monument['listcat']) { ?>
         <ul class="listes">
           <?php do { ?>
@@ -315,19 +380,32 @@ mysql_data_seek($ressource, 0);}
       <p>Il n'y a aucun objectif validé, pour le moment...</p>
       <?php }?>
         <br>
-        <p><strong>Influence sur l'économie :</strong></p>
+
+        <h3 class="souligne" style="margin-top: 0; border-color:#1a2638">Influence sur l'économie</h3>
              <div><?php
                 renderElement('temperance/resources', array(
                 'resources' => $row_monument_ressources
              ));
             ?></div>
+            <?php if ($coefficient < 1 ) { ?>
+            <div style="border: 5px dotted <?php if ($coefficient == 0.5 ) { ?>#cacaca<?php } else { ?>#1A2638<?php }?>;margin: 1em;padding: 0.5em 2em 1.5em; text-align: center;border-radius: 20px;">
+
+              <?php if ($coefficient == 0.5 ) { ?>
+                <h4>Cette entreprise n'a pas ajouté de nouvelle actualité depuis le <?php echo affDate($row_monument['ch_pat_mis_jour']); ?></h4><div><?= __s($row_monument['ch_pat_nom']) ?> n'a pas eu de nouveauté depuis plus de 3 mois,<br>
+                ses gains en ressources sont donc réduits de 50% pour privilégier les entreprises plus actives !</div>
+              <?php } elseif ($coefficient == 0.1 ) { ?>
+                <h4>Cette entreprise n'a pas ajouté de nouvelle actualité depuis le <?php echo affDate($row_monument['ch_pat_mis_jour']); ?></h4><div><?= __s($row_monument['ch_pat_nom']) ?> n'a pas eu de nouveauté depuis plus de 6 mois,<br>
+                ses gains en ressources sont donc réduits de 90% pour privilégier les entreprises actives !</div>
+              <?php }?>
+            </div><?php }?>
+<?php } ?>
       </div>
   </div>
 </div>
 <!-- Commentaire
         ================================================== -->
   <?php if ($_SESSION['statut'] >= 20) { ?>
-      <div class="pull-right-cta cta-title"> 
+      <div class="pull-right-cta cta-title">
           <a href="php/patrimoine-ajouter-monument-a-categorie-direct-modal.php?mon_id=<?= e($row_monument['ch_pat_id']) ?>" data-toggle="modal" data-target="#Modal-Monument" title="Modifier les catégories" class="btn btn-primary btn-cta" style="margin-top: -0.3em;">Modifier les catégories</a></div>
   <?php } ?>
 <section>
@@ -358,19 +436,20 @@ mysql_data_seek($ressource, 0);}
 
 <!-- Boite à outil du GO! -->
 <?php if ($row_users['ch_use_id'] == $_SESSION['user_ID']) { ?>
-<!--  C'est une entreprise-->
+<!--  C'est une entreprise -->
 <?php if($row_monument['ch_mon_cat_statut'] == 0) { ?>
+<?php if(is_null($row_mon_cat_GO_0) == false) { ?>
 
 <div class="accordion-group">
       <div class="accordion-heading guide-boite">
         <a class="accordion-toggle" data-toggle="collapse" href="#cat0"><h4 style="margin-top: 0px; margin-bottom: 0px; text-transform: uppercase; font-weight: lighter; color:#101010; "><div class="external-link-icon"
                  style="background-image:url('https://image.flaticon.com/icons/png/512/807/807313.png'); margin: 0em 0.2em 0.2em 0em;"></div> La boîte à Outils du GO!</h4></a>
       </div>
-        <div id="cat0" class="accordion-body collapse">
+        <div id="cat0" class="accordion-body ">
           <div class="accordion-inner" style="background: #f0eeec; border-left: 5px solid #ffcd00;">
       <ul  class="listes">
      <?= __s($row_monument['ch_pat_nom']) ?> peut encore se développer, voici quelques idées d'objectifs que tu pourrais essayer d'atteindre :
-   
+
     <div class="slider" style="padding-top: 45%;">
     <input type="radio" name="slider" title="slide1" checked="checked" class="slider__nav"/>
     <input type="radio" name="slider" title="slide2" class="slider__nav"/>
@@ -380,9 +459,8 @@ mysql_data_seek($ressource, 0);}
     <input type="radio" name="slider" title="slide6" class="slider__nav"/>
     <input type="radio" name="slider" title="slide7" class="slider__nav"/>
 
-
     <div class="slider__inner" style="width: 700%;">
-          
+
     <div class="slider__contents"><i class="slider__image fa fa-codepen"></i>
       <h2 class="slider__caption">En avant les présentations !</h2>
       <div class="slider__txt">
@@ -394,11 +472,9 @@ mysql_data_seek($ressource, 0);}
                     <div style="scale: 85%; margin-left: -4em;  margin-bottom: -0.5em; margin-top: -0.5em; position: initial; text-align: initial;"><img src="assets/img/ressources/budget.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Budget"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_budget']) ?></strong>  <img src="assets/img/ressources/industrie.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Industrie"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_industrie']) ?></strong>  <img src="assets/img/ressources/bureau.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Commerce"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_commerce']) ?></strong>  <img src="assets/img/ressources/agriculture.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Agriculture"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_agriculture']) ?></strong>  <img src="assets/img/ressources/tourisme.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Tourisme"><strong> <?= e($row_mon_cat_GO_0['ch_mon_cat_tourisme']) ?></strong>  <img src="assets/img/ressources/recherche.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Recherche"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_recherche']) ?></strong>  <img src="assets/img/ressources/environnement.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Evironnement"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_environnement']) ?></strong>  <img src="assets/img/ressources/education.png" style="max-width: 15px; position: inherit; margin: auto;" alt="icone Education"> <strong><?= e($row_mon_cat_GO_0['ch_mon_cat_education']) ?></strong>
                   </div></p></div>
             </li><?php } else { ?><?php }?>
-      <?php } while ($row_mon_cat_GO_0 = mysql_fetch_assoc($mon_cat_GO_0)); ?>
+      <?php } while ($row_mon_cat_GO_0 = mysqli_fetch_assoc($mon_cat_GO_0)); ?>
       </div>
     </div>
-    
-
     <div class="slider__contents"><i class="slider__image fa fa-codepen"></i>
       <h2 class="slider__caption">Un pour tous, tous pour un</h2>
       <div class="slider__txt"><?php if($nb_mon_cat_GO_310 == 6) { ?><span style="font-weight: bold; color: #ff4e00;">Tu peux dôter <?= __s($row_monument['ch_pat_nom']) ?> d'une <a target="_blank" href="http://vasel.yt/wiki/index.php?title=GO/Entreprise#Conditions_de_travail">politique sociale</a> reprenant l'une des 6 classes suivantes, numérotées de A à F.<br> Alors, plutôt meilleur ami de tes employés ou au contraire boss tyranique prêt à sacrifier ses troupes pour de l'industrie et du budget ?</span><?php } else { ?>Rien n'est jamais figé dans la vie d'une entreprise,<br> tu peux donc demander à tout moment de changer de classe en matière sociale que ce soit pour améliorer le sort de tes employés... ou pas !<?php }?>
@@ -490,15 +566,22 @@ mysql_data_seek($ressource, 0);}
 </div>
 
 <div style="padding-top: 1em;">Pense bien à déjà faire tes présentations sur le <a target="_blank" href="<?= __s($row_monument['ch_pat_legende_img5']) ?>">sujet de ton entreprise</a> sur le forum,<br> puis de demander ensuite juste en dessous à ce que ton objectif soit validé par le Comité Politique !</div><a target="_blank" href="http://vasel.yt/wiki/index.php?title=GO/Entreprise" class="guide-link">Envie de rentrer dans les détails ? GO!</a>
-
     </ul>
                 <div class="clearfix"></div>
             </div>
             </div>
 </div>
-<?php } while ($row_mon_cat_GO_0 = mysql_fetch_assoc($mon_cat_GO_0)); ?>
+<?php } else { ?>
+      <div><div class="guide-boite" style="padding: 0.5em">
+      <h4 style="margin-top: 0px; margin-bottom: 0px; text-transform: uppercase; font-weight: lighter; color:#101010; "><div class="external-link-icon" style="background-image:url('https://image.flaticon.com/icons/png/512/807/807313.png'); margin: 0em 0.2em 0.2em 0em;"></div> La boîte à Outils du GO!</h4>
+      <div>Avant de pleinement te lancer dans le développement de ton entreprise, tu dois déjà te faire valider les connexions entre cette page et les autres plateformes du Monde GC où <?= __s($row_monument['ch_pat_nom']) ?> est présente.
+        <form action="<?= DEF_URI_PATH ?>back/monument_modifier.php" method="post" style="margin: 1em 0em 0.6em;">
+    <input name="monument_ID" type="hidden" value="<?= e($row_monument['ch_pat_id']) ?>">
+    <button class="btn btn-primary" type="submit" title="Modifier cette entreprise"><i class="icon-pencil icon-white"></i>  Ajouter les liens</button>
+  </form></div>
+      </div><?php } ?>
 
-  <?php } ?>
+<?php } ?><?php } ?>
   </div>
   </div>
 
