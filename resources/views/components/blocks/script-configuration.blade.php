f0e057c6eacaba14d6e5895c92b7096e76302686
Roxel Lay <contact@romukulot.fr>
2021-09-20 18:28:16 +0200

    Meilleure génération des URLs vers l'endpoint des notifications
    Refactorise la variable globale MondegcData (window.*)
    
    Regénère les assets JS (npm run dev)

--- resources/views/components/blocks/script-configuration.blade.php
+++ resources/views/components/blocks/script-configuration.blade.php
@@ -1,3 +1,3 @@
 <script>
-    var MondegcData = {!! json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT) !!};
+    window.MondegcData = {!! json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT) !!};
 </script>
