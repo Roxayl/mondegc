d16a75af254bf571604090321a1f008aeb763714
Roxel Lay <contact@romukulot.fr>
2021-10-26 17:45:16 +0200

    Supprime les migrations (Laravel et legacy) au profit d'un dump du schéma

--- legacy/back/migration/migration-014.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$migration = new \App\Services\LegacyMigrationService();
-
-$migration->addQuery('alter table infrastructures
-	add judged_at datetime null after ch_inf_commentaire_juge');
-
-$migration->run();
