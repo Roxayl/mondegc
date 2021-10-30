42e344f2db2f753ef373be93f5d8c2ccae55297d
Roxel Lay <contact@romukulot.fr>
2021-10-30 23:47:43 +0200

    Déplace .docker vers docker + suppr à AWS dans la config

--- config/services.php
+++ config/services.php
@@ -24,12 +24,6 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
     'sparkpost' => [
         'secret' => env('SPARKPOST_SECRET'),
     ],
