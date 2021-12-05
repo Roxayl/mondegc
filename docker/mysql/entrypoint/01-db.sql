74d2af5687e6df23a2cb6280485a543cc20a2134
Roxel Lay <contact@romukulot.fr>
2021-12-05 16:17:30 +0100

    Fix entrypoint MySQL + code cleanup

--- docker/mysql/entrypoint/01-db.sql
+++ docker/mysql/entrypoint/01-db.sql
@@ -1,7 +1,3 @@
 # Créer les bases de données (dev et test).
 CREATE DATABASE IF NOT EXISTS `mondegc`;
 CREATE DATABASE IF NOT EXISTS `mondegc_testing`;
-
-# Créer l'utilisateur root et octroyer les accès.
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
