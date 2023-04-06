5d18c6be1fec3f0e8abb7700eec966411ddbba61
Roxel Lay <contact@romukulot.fr>
2023-04-06 19:57:45 +0200

    Intègre numéro de version/build dans .env

--- config/legacy.php
+++ config/legacy.php
@@ -5,13 +5,6 @@ use Illuminate\Support\Str;
 return [
 
     /*
-     * Version de l'application.
-     * Ce paramètre est notamment ajouté à la fin des URLs d'assets (fichiers CSS, JS...) afin de forcer le navigateur
-     * à ignorer le cache.
-     */
-    'version' => env('LEGACY_VERSION', '2.x'),
-
-    /*
      * Masque les erreurs PHP, en exécutant <code>error_reporting(0)</code>.
      */
     'hide_errors' => env('LEGACY_HIDE_ERRORS', false),
