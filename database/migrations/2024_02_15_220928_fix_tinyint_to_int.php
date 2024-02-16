ce2a8c7c7b3e413e97c8093f167ebb1cc70fd199
Roxel Lay <contact@romukulot.fr>
2024-02-16 11:54:37 +0100

    Fix "incorrect datetime value" pr dates fait histo

--- database/migrations/2024_02_15_220928_fix_tinyint_to_int.php
+++ database/migrations/2024_02_15_220928_fix_tinyint_to_int.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class FixTinyintToInt extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::statement('UPDATE `histoire` SET ch_his_date_fait = "0001-01-01 00:00:00"'
+            . ' WHERE ch_his_date_fait IS NULL OR ch_his_date_fait = "0000-00-00 00:00:00"');
+        DB::statement('UPDATE `histoire` SET ch_his_date_fait2 = NULL'
+            . ' WHERE ch_his_date_fait2 = "0000-00-00 00:00:00"');
+
         Schema::table('histoire', function (Blueprint $table) {
             $table->integer('ch_his_paysID')->change();
         });
@@ -30,11 +35,11 @@ return new class extends Migration
     public function down()
     {
         Schema::table('histoire', function (Blueprint $table) {
-            $table->tinyInteger('ch_his_paysID')->change();
+            $table->smallInteger('ch_his_paysID')->change();
         });
 
         Schema::table('patrimoine', function (Blueprint $table) {
-            $table->tinyInteger('ch_pat_paysID')->change();
+            $table->smallInteger('ch_pat_paysID')->change();
         });
     }
-};
+}
